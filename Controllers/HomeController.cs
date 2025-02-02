105a83f11040653f93a2451d3f9c91509f7abf7d
malmossa <malmossa@gmail.com>
2025-02-01 20:55:48 -0800

    completed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,6 +24,40 @@ namespace FizzBuzz_MVC.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(FizzBuzz model)
+        {
+            List<string> fbItems = new List<string>();
+
+            bool fizz;
+            bool buzz;
+
+            for(int i = 1; i <= 100; i++)
+            {
+                fizz = (i % model.FizzValue == 0);
+                buzz = (i % model.BuzzValue == 0);
+
+                if (fizz == true && buzz == true)
+                {
+                    fbItems.Add("FizzBuzz");
+                } else if (fizz == true)
+                {
+                    fbItems.Add("Fizz");
+                } else if (buzz == true)
+                {
+                    fbItems.Add("Buzz");
+                } else
+                {
+                    fbItems.Add(i.ToString());
+                }
+            }
+
+            model.Results = fbItems;
+
+            return View(model);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
